c48fb5bdc6507b6266aec6532f9b28e250dc6265
DovydasSp <ltdanis77@gmail.com>
2020-02-13 12:26:43 +0200

    Walking skeleton fixes

--- src/main/java/UseCaseFactoryImpl.java
+++ src/main/java/UseCaseFactoryImpl.java
@@ -12,7 +12,6 @@ public class UseCaseFactoryImpl implements UseCaseFactory {
     }
 
     public GenerateNumberUseCase buildGenerateNumberInteractor() {
-        NumberGateway gateway = new RandomNumberGateway();
         return new GenerateNumberInteractor(numberGateway);
     }
 
