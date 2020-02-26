277d85651f46a1b9b926cb9e351ee31a2e86d9c9
DovydasSp <ltdanis77@gmail.com>
2020-02-26 14:39:18 +0200

    Fixes

--- gateway/implementation/src/main/java/eu/openg/guessnumberapi/gateway/implementation/PostgreSQL/QueryUtils.java
+++ gateway/implementation/src/main/java/eu/openg/guessnumberapi/gateway/implementation/PostgreSQL/QueryUtils.java
@@ -6,16 +6,16 @@ class QueryUtils {
     static final String GUESS_COUNT = "guess_count";
     static final String ACTUAL_NUMBER = "actual_number";
 
-    static final String CREATE_TABLE_QUERY = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME +
-            "(" + GAME_ID + "        serial PRIMARY KEY NOT NULL, " +
-            GUESS_COUNT + "       int DEFAULT 0 NOT NULL, " +
-            ACTUAL_NUMBER + "     int NOT NULL);";
+    static final String CREATE_TABLE_QUERY = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + "(" +
+            GAME_ID + "        serial PRIMARY KEY NOT NULL, " +
+            GUESS_COUNT + "    int DEFAULT 0 NOT NULL, " +
+            ACTUAL_NUMBER + "  int NOT NULL);";
 
-    static final String INSERT_GAME_QUERY = "INSERT INTO " + TABLE_NAME + " (" + GUESS_COUNT + "," + ACTUAL_NUMBER + ") " +
-            "VALUES (?,?) RETURNING *;";
+    static final String INSERT_GAME_QUERY = "INSERT INTO " + TABLE_NAME + " (" + GUESS_COUNT + ","
+            + ACTUAL_NUMBER + ") VALUES (?,?) RETURNING *;";
 
-    static final String UPDATE_GUESSCOUNT_QUERY = "UPDATE " + TABLE_NAME + " SET " + GUESS_COUNT + " = " + GUESS_COUNT + " + 1 " +
-            "WHERE " + GAME_ID + " = ? RETURNING *;";
+    static final String UPDATE_GUESSCOUNT_QUERY = "UPDATE " + TABLE_NAME + " SET " + GUESS_COUNT + " = "
+            + GUESS_COUNT + " + 1 WHERE " + GAME_ID + " = ? RETURNING *;";
 
     static final String SELECT_GAME_QUERY = "SELECT * FROM " + TABLE_NAME + " WHERE " + GAME_ID + " = ?;";
 }
